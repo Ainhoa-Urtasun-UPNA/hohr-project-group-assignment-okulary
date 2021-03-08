1522fa6f6a389c712a46acc9cdb9f60292d3f9b6
Miguel Maeztu <maeztu.131676@e.unavarra.es>
2021-03-08 14:54:36 +0000

    push Run botton and it gives us: 0.7666667 0.5 1.92

--- HRplanning.py
+++ HRplanning.py
@@ -1,8 +1,8 @@
 import numpy
 
 # Forecasting number of available workers
-D = numpy.array([[15 0 0],[0 20 0],[0 0 45]])
-M = numpy.array([[5 2 0 ],[0 10 0],[5 5 45]])
+D = numpy.array([[15,0,0],[0,20,0],[0,0,45]])
+M = numpy.array([[5,2,0],[0,10,0],[5,5,45]])
 b = numpy.array([[2],[1],[1]])
 a = M@numpy.linalg.inv(D)@b
 print(a)
