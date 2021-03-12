b721c2b5618eb2d98be318149872e82c0380daa9
Miguel Maeztu <maeztu.131676@e.unavarra.es>
2021-03-12 16:20:33 +0000

    I wrote the matrices X and Y in Python

--- HRplanning.py
+++ HRplanning.py
@@ -6,11 +6,11 @@ M = numpy.array([[5,2,0],[0,10,0],[5,5,45]])
 b = numpy.array([[2],[1],[1]])
 a = M@numpy.linalg.inv(D)@b
 print(a) 
-The predicted number of employees is 1 manager 1 eingenieer and 2 operators.
+
 
 # Forecasting number of needed workers
-X = numpy.array([[],[],[],[],[],[],[],[],[]])
-Y = numpy.array([[],[],[],[],[],[],[],[],[]])
+X = numpy.array([[15,1,0,0],[17,1,0,0],[14,1,0,0],[6,0,1,0],[6,0,1,0],[5,0,1,0],[23,0,0,1],[27,0,0,1],[25,0,0,1]])
+Y = numpy.array([[4],[6],[5],[13],[12],[13],[7],[7],[9]])
 XTX = numpy.matmul(X.T,X)
 print(XTX)
 inverse = numpy.linalg.inv(XTX)
